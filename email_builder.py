7f6af5dcec3cac7fe085e64336019643cc76af18
SiGm4 <dimgmx@hotmail.com>
2023-02-13 18:01:54 +0200

    Added second email & link_exchange optionality

--- email_builder.py
+++ email_builder.py
@@ -1,14 +1,16 @@
 import re
+from datetime import datetime
+import calendar
 
-def molly_email(recipient_name, target_url, topic, our_url, anchor_text):
-    domain_regex = "^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/\n]+)"
+domain_regex = "^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/\n]+)"
 
+def molly_email(recipient_name, target_url, topic, our_url, anchor_text, link_exchange):
     paragraphs = []
     paragraphs.append("Hey " + recipient_name + ",")
-    paragraphs.append("Dimitris here from the content team at memberstack.com (DR 72).")
+    paragraphs.append("Dimitris here from the content team at memberstack.com (DR 73).")
     paragraphs.append("I'm reaching out to chat about a back-linking opportunity between memberstack.com and " + re.findall(domain_regex, target_url, flags = re.I | re.M)[0] + ". Is that something you've done in the past or are excited to learn more about?")
-    paragraphs.append("I get a fair few of these emails myself so totally understand if you hit archive. You won't get any follow-up emails from me 🤘")
-    paragraphs.append("I'm particularly interested in a blog post you have on " + topic + ". In it you mention '" + anchor_text + "' - we have a detailed post on exactly this, would you consider linking out to the Memberstack blog?")
+    paragraphs.append("I get a fair few of these emails myself so totally understand if you hit archive 🤘")
+    paragraphs.append("I'm particularly interested in a blog post you have on " + topic + ". In it you mention '" + anchor_text + "' - we have a detailed post on this, would you consider linking out to the Memberstack blog?")
     paragraphs.append("Here is the link for your convenience:")
     paragraphs.append(our_url)
     paragraphs.append("I'm wary of information overload so I won't yet go into the benefits those links can provide the both of us - but absolutely happy to chat about it more.")
@@ -17,5 +19,24 @@ def molly_email(recipient_name, target_url, topic, our_url, anchor_text):
 
     return "\n\n".join(paragraphs)
 
-def duncan_email():
-    pass
+def duncan_email(recipient_name, target_url, topic, our_url, anchor_text, link_exchange):
+    paragraphs = []
+    paragraphs.append("Hey " + recipient_name + ", happy " + calendar.day_name[datetime.today().weekday()] + " 👋")
+    paragraphs.append("Do you make updates to blog posts on " + re.findall(domain_regex, target_url, flags = re.I | re.M)[0] + " based on suggestions?")
+    paragraphs.append("✅ If yes… I'm on the content team at memberstack.com (DR 73) and I'm trying to get our blog posts in front of more people. We have 2,000+ customers including American Airlines, Slack, and Entreprenuer.com and they used us to build web apps with Webflow.")
+    paragraphs.append("❌ If not, you can archive this email 🤘")
+    paragraphs.append("Would you be willing to mention Memberstack in your article on " + topic + "? If yes, I can suggest adding it where you mention '" + anchor_text + "'.")
+    paragraphs.append("We have a detailed post on this, which you can find here:")
+    paragraphs.append(our_url)
+    if (link_exchange == "True"):
+        paragraphs.append("And I'd like to return the favor in some way. If you share a few of your business goals I can brainstorm ways to make this worth the effort (social share, backlink, collab on a blog post, etc.).")
+    else:
+        paragraphs.append("And I'd like to return the favor in some way - just let me know if there is anything I can do for you.")
+    paragraphs.append("Looking forward to your response!")
+    paragraphs.append("Thank you,")
+    paragraphs.append("Dimitris - Memberstack.com")
+
+    return "\n\n".join(paragraphs)
+
+#print(molly_email("Dimitris", "https://example.com/blog", "Custom Code in Webflow", "https://www.memberstack.com/blog/using-custom-code-in-webflow", "custom code", False))
+#print(duncan_email("Dimitris", "https://example.com/blog", "Custom Code in Webflow", "https://www.memberstack.com/blog/using-custom-code-in-webflow", "custom code", False))
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import sheetslib as sl
 
 from email_builder import molly_email
+from email_builder import duncan_email
 
 # import sheetsformatlib as sfl
 
@@ -11,9 +12,9 @@ from email_builder import molly_email
 # from googleapiclient.errors import HttpError
 
 # The ID of the main spreadsheet
-spreadsheet_id = '1_TFwhDWEbNOHqyUy_6GDtEEjdTTpbsCrc9QI67nP3Zo'
+spreadsheet_id = '1mLInhGR4zrkH2BbNZBEtyTqfZuV33od2ZG_JmaMDpOQ' #TOEDIT
 
-our_link = "https://www.memberstack.com/blog/cloneable-webflow-parallax-animation-templates"
+our_link = "https://www.memberstack.com/blog/using-custom-code-in-webflow" #TOEDIT
 
 if __name__ == '__main__':
     creds, service = sl.authorize()
@@ -28,7 +29,7 @@ df = pd.DataFrame(response[1:], columns=response[0])
 
 print(df.info())
 
-df["Email"] = df.apply(lambda row: molly_email(row["Recipient"], row["Content URL"], row["Topic"], our_link, row["Anchor Text"]), axis="columns")
+df["Email"] = df.apply(lambda row: duncan_email(row["Recipient"], row["Content URL"], row["Topic"], our_link, row["Anchor Text"], row["Reciprocical"]), axis="columns")
 
 print(df.tail())
 
@@ -37,4 +38,4 @@ print(df["Recipient"].values.tolist())
 body = df["Email"].values.tolist()
 body = [[item] for item in body]
 
-sl.update_values(spreadsheet_id, "Link Building Prospects!P2:P26", "USER_ENTERED", body, service)
+sl.update_values(spreadsheet_id, "Link Building Prospects!P2:P43", "USER_ENTERED", body, service) #TOEDIT
